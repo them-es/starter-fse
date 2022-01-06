d33b828f4a54eaaa186787c353b2b4ba4c642843
them-es <them-es@users.noreply.github.com>
2022-01-06 07:08:49 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require( 'path' ),
+	// Modify "dev_url" to actual localhost url
+	dev_url = 'http://themes.localhost/starter-fse';
+
+module.exports = {
+	context: path.resolve( __dirname, 'assets' ),
+	entry: [
+		'./main.scss',
+		'./main.js',
+	],
+	output: {
+		path: path.resolve( __dirname, 'assets/js' ),
+		filename: '[name].bundle.js',
+	},
+	//devtool: 'source-map',
+	watch: true,
+	devServer: {
+		proxy: {
+			'*': {
+				target: dev_url,
+				changeOrigin: true,
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'main.css',
+						},
+					},
+					{ loader: 'extract-loader' },
+					{ loader: 'css-loader' },
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [
+									require( 'autoprefixer' )
+								]
+							},
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							implementation: require( 'sass' ),
+							webpackImporter: false, // See https://github.com/webpack-contrib/sass-loader/issues/804
+							sassOptions: {
+								includePaths: [ 'node_modules' ],
+							},
+						},
+					}
+				]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i, 
+				loader: 'file-loader',
+				options: {
+					esModule: false,
+					outputPath: './assets/img',
+					publicPath: '../img',
+					name: '[name].[ext]',
+				},
+			},
+		]
+	},
+};
