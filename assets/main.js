b626313c7375f2c28e60a11d1d0d054d3b68f322
them-es <them-es@users.noreply.github.com>
2022-10-17 13:50:20 +0200

    New release v1.1.1
    Autoclose header nav if modal is open and browser window gets resized
    Responsive breakpoints as variable
    theme.json bugfixes

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,21 @@
 ( function () {
 	'use strict';
 
+	// Switching to mobile: https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/onchange
+	const isMobile = window.matchMedia( '(max-width: ' + getComputedStyle( document.body ).getPropertyValue( '--custom--media-max-width--sm' ) + ')' );
+	const navigationResponsiveContainer = document.querySelector( '.site-header .wp-block-navigation__responsive-container' );
+	isMobile.onchange = ( e ) => {
+		if ( e.matches ) {
+			// <= Mobile
+		} else {
+			// > Mobile
+			// Autoclose header nav container if modal is open and browser window gets resized.
+			if ( document.body.contains( navigationResponsiveContainer ) && navigationResponsiveContainer.classList.contains( 'is-menu-open' ) ) {
+				document.querySelector( '.site-header .wp-block-navigation__responsive-container-close' ).click();
+			}
+		}
+	}
+
 	// Style password protected post form.
 	const passwordButton = document.querySelector( '.post-password-form [type="submit"]' );
 	if ( document.body.contains( passwordButton ) ) {
