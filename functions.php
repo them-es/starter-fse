50c1a7488d7959f6ad77fc3c28e4a1bd204e57c0
them-es <them-es@users.noreply.github.com>
2026-06-17 16:36:13 +0200

    Tested up to WordPress 7.0
    Added "Contrast" color to default palette
    Minor bugfixes
    Code refactoring

--- functions.php
+++ functions.php
@@ -1,51 +1,49 @@
 <?php
-if ( ! function_exists( 'themes_starter_theme_support' ) ) {
-	/**
-	 * General Theme Settings.
-	 *
-	 * @since v1.0
-	 *
-	 * @return void
-	 */
-	function themes_starter_theme_support() {
-		// Make theme available for translation: Translations can be filed in the /languages/ directory.
-		load_theme_textdomain( 'my-theme', __DIR__ . '/languages' );
+/**
+ * General Theme Settings.
+ *
+ * @since v1.0
+ *
+ * @return void
+ */
+function themes_starter_theme_support() {
+	// Make theme available for translation: Translations can be filed in the /languages/ directory.
+	load_theme_textdomain( 'my-theme', __DIR__ . '/languages' );
 
-		// Add support for Post thumbnails.
-		add_theme_support( 'post-thumbnails' );
-		// Add support for responsive embedded content.
-		add_theme_support( 'responsive-embeds' );
-		// Add support for Block Styles.
-		add_theme_support( 'wp-block-styles' );
+	// Add support for Post thumbnails.
+	add_theme_support( 'post-thumbnails' );
+	// Add support for responsive embedded content.
+	add_theme_support( 'responsive-embeds' );
+	// Add support for Block Styles.
+	add_theme_support( 'wp-block-styles' );
 
-		// Add support for Editor Styles.
-		add_theme_support( 'editor-styles' );
-		// Enqueue Editor Styles.
-		add_editor_style(
-			'style-editor.css'
-		);
-	}
-	add_action( 'after_setup_theme', 'themes_starter_theme_support' );
-
-	/**
-	 * Enqueue editor stylesheet (for iframed Post Editor):
-	 * https://make.wordpress.org/core/2023/07/18/miscellaneous-editor-changes-in-wordpress-6-3/#post-editor-iframed
-	 *
-	 * @since v1.2.2
-	 *
-	 * @return void
-	 */
-	function themes_starter_load_editor_styles() {
-		if ( is_admin() ) {
-			wp_enqueue_style( 'editor-style', get_theme_file_uri( 'style-editor.css' ) );
-		}
-	}
-	add_action( 'enqueue_block_assets', 'themes_starter_load_editor_styles' );
-
-	// Disable Block Directory: https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/filters/editor-filters.md#block-directory
-	remove_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets' );
-	remove_action( 'enqueue_block_editor_assets', 'gutenberg_enqueue_block_editor_assets_block_directory' );
+	// Add support for Editor Styles.
+	add_theme_support( 'editor-styles' );
+	// Enqueue Editor Styles.
+	add_editor_style(
+		'style-editor.css'
+	);
 }
+add_action( 'after_setup_theme', 'themes_starter_theme_support' );
+
+/**
+ * Enqueue editor stylesheet (for iframed Post Editor):
+ * https://make.wordpress.org/core/2023/07/18/miscellaneous-editor-changes-in-wordpress-6-3/#post-editor-iframed
+ *
+ * @since v1.2.2
+ *
+ * @return void
+ */
+function themes_starter_load_editor_styles() {
+	if ( is_admin() ) {
+		wp_enqueue_style( 'editor-style', get_theme_file_uri( 'style-editor.css' ) );
+	}
+}
+add_action( 'enqueue_block_assets', 'themes_starter_load_editor_styles' );
+
+// Disable Block Directory: https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/filters/editor-filters.md#block-directory
+remove_action( 'enqueue_block_editor_assets', 'wp_enqueue_editor_block_directory_assets' );
+remove_action( 'enqueue_block_editor_assets', 'gutenberg_enqueue_block_editor_assets_block_directory' );
 
 /**
  * Custom Template part.
@@ -70,25 +68,23 @@ function themes_starter_custom_template_part_area( $areas ) {
 }
 add_filter( 'default_wp_template_part_areas', 'themes_starter_custom_template_part_area' );
 
-if ( ! function_exists( 'themes_starter_load_scripts' ) ) {
-	/**
-	 * Enqueue CSS Stylesheets and Javascript files.
-	 *
-	 * @return void
-	 */
-	function themes_starter_load_scripts() {
-		$theme_version = wp_get_theme()->get( 'Version' );
+/**
+ * Enqueue CSS Stylesheets and Javascript files.
+ *
+ * @return void
+ */
+function themes_starter_load_scripts() {
+	$theme_version = wp_get_theme()->get( 'Version' );
 
-		// 1. Styles.
-		wp_enqueue_style( 'style', get_stylesheet_uri(), array(), $theme_version );
-		wp_enqueue_style( 'main', get_theme_file_uri( 'build/main.css' ), array(), $theme_version, 'all' ); // main.scss: Compiled custom styles.
+	// 1. Styles.
+	wp_enqueue_style( 'style', get_stylesheet_uri(), array(), $theme_version );
+	wp_enqueue_style( 'main', get_theme_file_uri( 'build/main.css' ), array(), $theme_version, 'all' ); // main.scss: Compiled custom styles.
 
-		if ( is_rtl() ) {
-			wp_enqueue_style( 'rtl', get_theme_file_uri( 'build/rtl.css' ), array(), $theme_version, 'all' );
-		}
-
-		// 2. Scripts.
-		wp_enqueue_script( 'mainjs', get_theme_file_uri( 'build/main.js' ), array(), $theme_version, true );
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'rtl', get_theme_file_uri( 'build/rtl.css' ), array(), $theme_version, 'all' );
 	}
-	add_action( 'wp_enqueue_scripts', 'themes_starter_load_scripts' );
+
+	// 2. Scripts.
+	wp_enqueue_script( 'mainjs', get_theme_file_uri( 'build/main.js' ), array(), $theme_version, true );
 }
+add_action( 'wp_enqueue_scripts', 'themes_starter_load_scripts' );
